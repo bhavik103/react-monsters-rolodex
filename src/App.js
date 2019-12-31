beafa92403b2cda3742593cbc78b95afe04f3224
bhavik103 <bhavikkalariya103@gmail.com>
2019-12-31 17:37:26 +0530

    search with email and name implemented

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends Component {
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter(monster =>
-      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      monster.name.toLowerCase().includes(searchField.toLowerCase()) || monster.email.toLowerCase().includes(searchField.toLowerCase())
     );
 
     return (
