b3866dd9cdab78b920de2bd0d4cf022cd013106a
bhavik103 <bhavikkalariya103@gmail.com>
2019-12-31 16:02:29 +0530

    Updates

---
+++ precache-manifest.6fd89cbfd8ef2e7636e90f1ab50e434e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "001b24a23626de6371359dcbe8e016e0",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e9bfb25decec56212580",
+    "url": "/react-monsters-rolodex/static/css/main.2f37f620.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/react-monsters-rolodex/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-monsters-rolodex/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e9bfb25decec56212580",
+    "url": "/react-monsters-rolodex/static/js/main.4fd977d5.chunk.js"
+  },
+  {
+    "revision": "6e7fdd83c2267585592c",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.4474d36d.js"
+  }
+]);
